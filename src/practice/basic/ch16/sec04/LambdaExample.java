6c0736dfc347b941524cbbe9c6733de3fc5668ac
kimjaehyeonn <jhjhh023@gmail.com>
2025-05-07 16:51:04 +0900

    lambda basic problem

--- src/practice/basic/ch16/sec04/LambdaExample.java
+++ src/practice/basic/ch16/sec04/LambdaExample.java
@@ -11,6 +11,18 @@ public class LambdaExample {
         });
 
         // sum 이용
+        /*메서드 참조
+        * 함수형 인터페이스를 람다식으로 쓰는게 아닌, 일반 메소드를 참조시켜 선언하는 방법
+        * 일반 메소드를 참조하기 위한 조건
+        * 각 메소드의 매개변수 타입/ 갯수/ 반환형이 같아야 함
+        *
+        * //static
+        * 클래스이름::메소드이름
+        *
+        * //instance
+        * 참조변수이름::메소드이름
+        **/
+        person.action((LambdaExample::sum));
         person.action((x,y)-> sum(x,y));
 
 
